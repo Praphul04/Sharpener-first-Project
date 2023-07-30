042dd234b74a8078d6b24c08e01f4395887f609a
Praphul04 <praphulthakur6@gmail.com>
2023-07-30 19:22:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -16,10 +16,12 @@ function App() {
   };
 
   return (
-    <div>
+    <React.Fragment>
+    
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    
+    </React.Fragment>
   );
 }
 
